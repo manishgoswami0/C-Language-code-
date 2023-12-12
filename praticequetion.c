aee0872ab4f39c7eb2643e066b72e95ba742d1ac
manishgoswami0 <152800516+manishgoswami0@users.noreply.github.com>
2023-12-12 23:57:29 +0530

    Add files via upload

---
+++ praticequetion.c
@@ -0,0 +1,115 @@
+// #include<stdio.h>
+// int main(){
+//     int x=2;
+//     if(x=1){
+//         pintf("valid");
+//     }
+//     else{
+//         printf("not valid");
+//     }
+//     return 0;
+
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int i =0;
+//     for (int i=0;i<=5;i++){
+//         printf("%d \n",i);
+//     }
+//     return 0 ;
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int i=1;
+//     printf("%d\n",i--);
+//     printf("%d\n",i);
+// }
+// #include<stdio.h>
+// int main(){
+//     int a;
+//     printf("enter the number :");
+//     scanf("%d",&a);
+//     // int i=0;
+//     // while (i<=a){
+//     //     printf("%d\n",i);
+//     //     i++;
+//     // }
+//     // for(int i=0;i<=a;i++){
+//     //     printf("%d\n",i);
+//     // }
+//     int i=0;
+//     do{
+//         printf("%d\n",i);
+//         i++;
+//     }while(i<=a);
+// }
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter the number: ");
+//     scanf("%d",&n);
+//     for (int i=10;i>=1;i--){
+
+//        printf("%d\n",n*i);
+
+//     }
+// }
+
+// #include<stdio.h>
+// void namaste();
+// void bonjour();
+// int main(){
+//     char ch;
+//     printf("Enter if you are indian press 'i' & not then press 'f' ");
+//     scanf("%c",&ch);
+//     if(ch=='i'){
+//         namaste();
+//     }
+//     else{
+//         bonjour();
+//     }
+//     return 0;
+// }
+
+// void namaste(){
+//     printf("namaste");
+// }
+
+// void bonjour(){
+//     printf("bonjour");
+// }
+
+// #include<stdio.h>
+
+// int sum (int a,int b);
+
+// int main(){
+//     int a,b;
+//     printf("Enter the number :");
+//     scanf("%d",&a);
+//     printf("Enter the number :");
+//     scanf("%d",&b);
+//     int total=sum(a,b);
+//     printf("sum of number %d",total);
+//     return 0;
+// }
+
+// int sum(int a,int b){
+//     return a+b;ṇ
+// }
+
+#include<stdio.h>
+#include<math.h>
+//void square ( number);
+
+int main (){
+    int n=4;
+    printf("%f",pow(n,2));
+    return 0;
+}
+
+// void square ( number ){
+
+// }
